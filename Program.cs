ed1902c3b08428758b0ba553a6fb47463acff6de
Murad <11_rma@mail.ru>
2022-03-30 14:37:22 +0300

    seminar2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10
+string number = Console.ReadLine();
+while (number.Length != 3)
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число ");
+    number = Console.ReadLine();
+}
+System.Console.WriteLine(number[1]);
