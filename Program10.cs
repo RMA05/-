06f5d5e9050f1b7a762410b284e759eb52fbd94d
RMA05 <99750255+RMA05@users.noreply.github.com>
2022-04-20 01:08:18 +0300

    Delete Program10.cs

--- Program10.cs
+++
@@ -1,8 +0,0 @@
-﻿// Задача 10
-string number = Console.ReadLine();
-while (number.Length != 3)
-{
-    System.Console.WriteLine("Вы ввели не трехзначное число ");
-    number = Console.ReadLine();
-}
-System.Console.WriteLine(number[1]);
